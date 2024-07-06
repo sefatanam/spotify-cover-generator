539b4ac7f7ffc0a35215412b32f16d41bc83a3a4
sefatanam <sefatanam@gmail.com>
2024-07-06 22:39:46 +0600

    remove file url

--- src/scripts/tools.ts
+++ src/scripts/tools.ts
@@ -76,7 +76,7 @@ export type FadeSettingProp = {
 }[keyof FadeSetting]
 
 export const defaultFadeSetting: FadeSetting = {
-    fileURL: "https://plus.unsplash.com/premium_photo-1689974465650-b223928cdc9e?q=80&w=3884&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    fileURL: "",
     text1: "",
     text2: "",
     color1: "#000000",
